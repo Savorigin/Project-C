738325312c46e3edc8398adb1a6e8178e4fc7350
Vladimir Savchenko <savorigin@outlook.com>
2017-12-10 22:04:38 +0200

    10/12/2017

--- Begin1-10/Begin1-10/Program.cs
+++ Begin1-10/Begin1-10/Program.cs
@@ -1,29 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.;
 
 namespace Begin1_10
 {
     class Program
     {
-        static void Main(string[] args)
+        static double Square()
         {
             Console.WriteLine("Введите сторону квадрата");
             string S1 = Console.ReadLine();
-            int a = int.Parse(S1);
-
-            // Begin1
-            /*int P = 4 * a;
-            Console.WriteLine("Периметр квадрата = " + P.ToString());*/
-
-            // Begin2
-            double S = Math.Pow((double)a, 2);
+            return double.Parse(S1);
+        }
+        static void Begin1()
+        {
+            double a = Square();
+            double P = 4 * a;
+            Console.WriteLine("Периметр квадрата = " + P.ToString());
+        }
+        static void Begin2()
+        {
+            double a = Square();
+            double S = Math.Pow(a, 2);
             Console.WriteLine("Площадь квадрата = " + S.ToString());
+        }
+        static void Begin3()
+        {
+            Console.WriteLine("Введите 1 сторону прямоугольника");
+            string S1 = Console.ReadLine();
+            Console.WriteLine("Введите 2 сторону прямоугольника");
+            string S2 = Console.ReadLine();
+            double a = double.Parse(S1);
+            double b = double.Parse(S2);
+            double S = a * b;
+            Console.WriteLine("Площадь прямоугольника = " + S.ToString());
+            double P = 2 * (a + b);
+            Console.WriteLine("Периметр прямоугольника = " + P.ToString());
+        }
+        static void Begin4()
+        {
+            double d = 15;
+            const double pi = 3.14;
+            double L = pi * d;
+            Console.WriteLine("Длина окружности = " + L.ToString());
+        }
+        static void Begin5()
+        {
+            double a = 23;
+            double V = Math.Pow(a, 3);
+            Console.WriteLine("Объем куба = " + V.ToString());
+            double S = 6 * Math.Pow(a, 2);
+            Console.WriteLine("Поверхность куба = " + S.ToString());
+        }
+        static void Begin6()
+        {
 
-            Console.ReadLine();
+        }
+        static void Begin7()
+        {
+
+        }
+        static void Begin8()
+        {
+
+        }
+        static void Begin9()
+        {
+
+        }
+        static void Begin10()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите задание:");
+            string nameTask = Console.ReadLine();
+
+            switch (nameTask)
+            {
+                case "1": Begin1(); break;
+                case "2": Begin2(); break;
+                case "3": Begin3(); break;
+                case "4": Begin4(); break;
+                case "5": Begin5(); break;
+                case "6": Begin6(); break;
+                case "7": Begin7(); break;
+                case "8": Begin8(); break;
+                case "9": Begin9(); break;
+                case "10": Begin10(); break;
+                default: Begin1(); break;
+            }
         }
     }
 }
---
+++ Integer1-10/Integer1-10/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integer1_10
+{
+    class Program
+    {
+        static void Integer1()
+        {
+            int L = 156;
+            int ost = L % 100;
+            double countM = ((L - ost) / 100);
+            Console.WriteLine("Количество метров = " + countM.ToString());
+        }
+        static void Integer2()
+        {
+            int kg = 1556546;
+            double t = Math.Floor((double)kg / 1000);
+            Console.WriteLine("Количество тонн = " + t.ToString());
+        }
+        static void Integer3()
+        {
+
+        }
+        static void Integer4()
+        {
+
+        }
+        static void Integer5()
+        {
+
+        }
+        static void Integer6()
+        {
+
+        }
+        static void Integer7()
+        {
+
+        }
+        static void Integer8()
+        {
+
+        }
+        static void Integer9()
+        {
+
+        }
+        static void Integer10()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер задания:");
+            string nameTask = Console.ReadLine();
+            switch (nameTask)
+            {
+                case "1": Integer1(); break;
+                case "2": Integer2(); break;
+                case "3": Integer3(); break;
+                case "4": Integer4(); break;
+                case "5": Integer5(); break;
+                case "6": Integer6(); break;
+                case "7": Integer7(); break;
+                case "8": Integer8(); break;
+                case "9": Integer9(); break;
+                case "10": Integer10(); break;
+                default: Integer1(); break;
+            }
+        }
+    }
+}
